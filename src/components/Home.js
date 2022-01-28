26ea98f25958546fcfcd71bc30c8cf64468f670c
Timjini <hatim.jini@gmail.com>
2022-01-29 00:43:15 +0300

    bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import './Home.css'
 import Help1 from '../images/help1.jpg'
 import banner from '../images/art-banner.jpg'
 import Help2 from '../images/help2.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Home () {
